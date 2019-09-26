2b890712036362cc5536f0ab31067909e8c0fa13
Riley <lannon.riley@gmail.com>
2019-09-26 16:19:06 -0500

    Add allocators to stack and queue classes
    The stack and queue classes now use C++ allocators to allocate a dynamic array rather than using std::vector behind the hood. Note that since allocator::construct and allocator::destroy are deprecated (and slated to be removed in C++20), allocator_traits is used for this functionality.

--- queue.h
+++ queue.h
@@ -2,79 +2,167 @@
 
 Algorithms and Data Structures
 Copyright 2019 Riley Lannon
-Queue.h
+queue.h
 
-An implementation of the Queue data structures using C++ templates
+An implementation of the Queue data structures using C++ templates and STL Allocators
 
 */
 
 #pragma once
 
-#include <vector>
-#include <exception>
+#include <memory>
 #include <stdexcept>
 
-template <typename T>
+template <typename T, typename Allocator = std::allocator<T>>
 class queue
 {
-	std::vector<T> data;
+	Allocator queue_allocator;
+
+	size_t _size;
+	size_t _capacity;
+
+	T* buffer;	// buffer total
 public:
-	void push(T const& elem);
+	size_t size() const;
+	size_t capacity() const;
+	bool empty() const;
 
-	T peek() const;
-	T pop();
+	void push_back(T to_push);
+	T peek_front();
+	T pop_front();
 
-	bool is_empty() const;
-
-	queue();
+	explicit queue();
 	~queue();
 };
 
-template <typename T>
-inline void queue<T>::push(T const& elem)
+/*
+
+Getters
+
+*/
+
+template<typename T, typename Allocator>
+inline size_t queue<T, Allocator>::size() const
 {
-	this->data.push_back(elem);
+	return this->_size();
+}
+
+template<typename T, typename Allocator>
+inline size_t queue<T, Allocator>::capacity() const
+{
+	return this->_capacity;
+}
+
+template<typename T, typename Allocator>
+inline bool queue<T, Allocator>::empty() const
+{
+	return this->_size == 0;
+}
+
+template<typename T, typename Allocator>
+inline void queue<T, Allocator>::push_back(T to_push)
+{
+	if (this->_capacity == 0)
+	{
+		this->buffer = this->queue_allocator.allocate(1);
+		this->_capacity = 1;
+	}
+	else if (this->_size == this->_capacity)
+	{
+		size_t new_capacity = this->_capacity * 1.5;
+		if (new_capacity < 4)
+		{
+			new_capacity *= 2;
+		}
+
+		// on a resize, we need to copy the contents
+		T* old_buf = this->buffer;
+		this->buffer = this->queue_allocator.allocate(new_capacity);
+
+		if (this->buffer)
+		{
+			memcpy(buffer, old_buf, this->_size * sizeof(T));
+			this->queue_allocator.deallocate(old_buf, this->_size);
+
+			this->_capacity = new_capacity;
+		}
+		else
+		{
+			throw std::runtime_error("Could not allocate memory");
+		}
+	}
+
+	// finally, perform the push
+	std::allocator_traits<Allocator>::construct(this->queue_allocator, &this->buffer[this->_size], to_push);
+	this->_size += 1;
+
 	return;
 }
 
-template <typename T>
-inline T queue<T>::peek() const
+template<typename T, typename Allocator>
+inline T queue<T, Allocator>::peek_front()
 {
-	if (this->data.empty())
+	if (this->_size != 0)
 	{
-		throw std::out_of_range("Queue<T> : cannot peek empty queue");
+		return this->buffer[this->_size - 1];
 	}
 	else
 	{
-		return this->data[0];
+		throw std::out_of_range("Cannot peek on empty queue");
 	}
 }
 
-template <typename T>
-inline T queue<T>::pop()
+template<typename T, typename Allocator>
+inline T queue<T, Allocator>::pop_front()
 {
-	if (this->data.empty())
+	T to_return;
+
+	if (this->_size > 0)
 	{
-		throw std::out_of_range("Queue<T> : cannot pop from empty queue");
+		// first, fetch the element
+		to_return = this->buffer[0];
+
+		// destroy the element and update the size
+		std::allocator_traits<Allocator>::destroy(this->queue_allocator, this->buffer);
+		this->_size -= 1;
+
+		// now, move all of the elements in the queue over
+		for (size_t i = 0; i < this->_size; i++)
+		{
+			this->buffer[i] = this->buffer[i + 1];
+		}
 	}
 	else
 	{
-		T to_return = this->data[0];
-		this->data.erase(this->data.begin());
-		return to_return;
+		throw std::out_of_range("Cannot pop from empty queue");
 	}
+
+	return to_return;
 }
 
-template <typename T>
-inline bool queue<T>::is_empty() const
+template<typename T, typename Allocator>
+inline queue<T, Allocator>::queue()
 {
-	return this->data.empty();
+	this->buffer = nullptr;
+	this->_capacity = 0;
+	this->_size = 0;
+	this->queue_allocator = Allocator();
 }
 
-template<class T> queue<T>::queue()
+template<typename T, typename Allocator>
+inline queue<T, Allocator>::~queue()
 {
+	if (this->_size != 0)
+	{
+		for (size_t i = 0; i < this->_size; i++)
+		{
+			std::allocator_traits<Allocator>::destroy(this->queue_allocator, &this->buffer[i]);
+			this->_size -= 1;
+		}
+	}
+
+	this->queue_allocator.deallocate(this->buffer, this->_capacity);
+	this->_capacity = 0;
+	this->buffer = nullptr;
 }
 
-template<class T> queue<T>::~queue()
-{
-}
--- stack.h
+++ stack.h
@@ -2,79 +2,178 @@
 
 Algorithms and Data Structures
 Copyright 2019 Riley Lannon
-Stack.h
+stack.h
 
-An implementation of the Stack data structure using C++ templates
+An implementation of the Stack data structures using C++ templates and STL Allocators
 
 */
 
 #pragma once
 
-#include <vector>
-#include <exception>
+#include <memory>
 #include <stdexcept>
 
-template <typename T>
+template <typename T, typename Allocator = std::allocator<T>>
 class stack
 {
-	std::vector<T> data;
+	Allocator stack_allocator;
+	size_t _size;
+	size_t _capacity;
+
+	T* buffer;
 public:
-	void push(T const& elem);
-	T pop();
-	T peek() const;
+	void push_back(T to_push);
+	T pop_back();
+	T peek();
 
-	bool is_empty() const;
+	size_t max_size() const;
+	size_t capacity() const;
+	size_t size() const;
+	bool empty() const;
 
-	stack();
+	explicit stack();
 	~stack();
 };
 
-template <typename T>
-inline void stack<T>::push(T const& elem)
+/*
+
+Getters
+
+*/
+
+template <typename T, typename Allocator>
+size_t stack<T, Allocator>::max_size() const
 {
-	this->data.push_back(elem);
+	return this->stack_allocator.max_size();
+}
+
+template <typename T, typename Allocator>
+size_t stack<T, Allocator>::capacity() const
+{
+	return this->_capacity;
+}
+
+template <typename T, typename Allocator>
+size_t stack<T, Allocator>::size() const
+{
+	return this->_size;
+}
+
+template <typename T, typename Allocator>
+bool stack<T, Allocator>::empty() const
+{
+	return this->_size == 0;
+}
+
+/*
+
+Push / Peek / Pop
+
+*/
+
+template <typename T, typename Allocator>
+void stack<T, Allocator>::push_back(T to_push)
+{
+	if (this->_capacity == 0)
+	{
+		this->buffer = this->stack_allocator.allocate(1);
+		this->_capacity = 1;
+	}
+	else if (this->_size == this->_capacity)
+	{
+		size_t new_capacity = this->_capacity * 1.5;
+
+		if (new_capacity < 4)
+		{
+			new_capacity *= 2;
+		}
+
+		T* old_buff_ptr = this->buffer;
+		this->buffer = this->stack_allocator.allocate(new_capacity);
+
+		if (this->buffer)
+		{
+			memcpy(this->buffer, old_buff_ptr, this->_size * sizeof(T));
+			this->stack_allocator.deallocate(old_buff_ptr, this->_size);
+
+			this->_capacity = new_capacity;
+		}
+		else
+		{
+			throw std::runtime_error("Cannot push back");
+		}
+	}
+
+	std::allocator_traits<Allocator>::construct(this->stack_allocator, &this->buffer[this->_size], to_push);
+	this->_size += 1;
+
 	return;
 }
 
-template <typename T>
-inline T stack<T>::pop()
+template <typename T, typename Allocator>
+T stack<T, Allocator>::pop_back()
 {
-	if (this->data.size() == 0)
-	{
-		throw std::out_of_range("Stack<T>: cannot pop from empty stack");
-	}
-	else {
-		T to_return = this->data.back();
-		this->data.pop_back();
-		return to_return;
-	}
-}
+	T to_return;
 
-template <typename T>
-inline T stack<T>::peek() const
-{
-	if (this->data.size() == 0)
+	if (this->_size == 0)
 	{
-		throw std::out_of_range("Stack<T>: cannot peek empty stack");
+		throw std::out_of_range("Cannot pop from empty stack");
 	}
 	else
 	{
-		return data.back();
+		to_return = this->buffer[this->_size - 1];
+		std::allocator_traits<Allocator>::destroy(this->stack_allocator, &this->buffer[this->_size - 1]);
+		this->_size -= 1;
+	}
+
+	return to_return;
+}
+
+template <typename T, typename Allocator>
+T stack<T, Allocator>::peek()
+{
+	// returns the top element of the Stack without popping it
+
+	if (this->_size != 0)
+	{
+		return this->buffer[this->size - 1];
+	}
+	else
+	{
+		throw std::out_of_range("Cannot peek on an empty stack");
 	}
 }
 
-template <typename T>
-inline bool stack<T>::is_empty() const
+/*
+
+Constructor and destructor
+
+*/
+
+template <typename T, typename Allocator>
+stack<T, Allocator>::stack()
 {
-	return this->data.empty();
+	this->stack_allocator = Allocator();
+	this->_size = 0;
+	this->_capacity = 0;
+	this->buffer = nullptr;
 }
 
-template <typename T>
-inline stack<T>::stack()
+template <typename T, typename Allocator>
+stack<T, Allocator>::~stack()
 {
-}
+	// if we still have objects in the stack, destroy them before deallocating the stack's memory
+	if (this->_size > 0)
+	{
+		for (size_t i = 0; i < this->_size; i++)
+		{
+			std::allocator_traits<Allocator>::destroy(this->stack_allocator, &this->buffer[i]);
+			this->_size -= 1;
+		}
+	}
 
-template <typename T>
-inline stack<T>::~stack()
-{
+	// deallocate the memory, set the capacity to 0, set the buffer to nullptr
+	this->stack_allocator.deallocate(this->buffer, this->_capacity);
+	this->_capacity = 0;
+	this->buffer = nullptr;
 }
