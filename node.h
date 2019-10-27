9f89b5940f4e15c5b7a81e030c14519e79b9485e
Riley <lannon.riley@gmail.com>
2019-10-27 13:55:08 -0500

    Added operator== for node<T> with T

--- node.h
+++ node.h
@@ -18,6 +18,10 @@ class node
 	T data;
 	node<T>* next;
 public:
+	// allow us to compare node<T> to T by checking the _data_ at the node with the righthand value
+	bool operator==(T right);
+	bool operator!=(T right);
+
 	T& get_data();
 	node<T>* get_next();
 
@@ -28,6 +32,18 @@ public:
 	node();
 	~node();
 };
+
+template <typename T>
+inline bool node<T>::operator==(T right)
+{
+	return this->data == right;
+}
+
+template <typename T>
+inline bool node<T>::operator!=(T right)
+{
+	return this->data != right;
+}
 
 template <typename T>
 inline void node<T>::set_data(T data)
